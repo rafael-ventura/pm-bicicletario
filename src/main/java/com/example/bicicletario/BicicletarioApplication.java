9c64315c4f4ed35912b550b923c6f9e24314de5e
Rafael Ventura <raf4ael@gmail.com>
2024-05-13 13:45:00 -0300

    commit inicial

---
+++ src/main/java/com/example/bicicletario/BicicletarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.bicicletario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicicletarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicicletarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bicicletario/BicicletarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bicicletario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicicletarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
