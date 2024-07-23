e9020835ea04590c554b2f1cc43b1e59c6ffbdf1
Rafael Ventura <raf4ael@gmail.com>
2024-07-23 07:05:50 -0300

    Logica para subir AWS com URL amigavel

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo chmod +x /home/ec2-user/bicicletario-0.0.1-SNAPSHOT.jar
+sudo chmod +x /home/ec2-user/bicicletario-equipamento.jar
--- src/main/java/com/example/bicicletario/MainApp.java
+++ src/main/java/com/example/bicicletario/MainApp.java
@@ -2,12 +2,19 @@ package com.example.bicicletario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class MainApp  {
+@Controller
+public class MainApp {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
     }
 
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/swagger.html";
+    }
 }
