600ad5dd1acfdaea11b00cc9917fd14d9992c0f5
indio <vitorindio@edu.unirio.br>
2024-07-24 18:56:27 -0300

    [aluguel]

--- src/main/java/com/example/bicicletario/bicicletario/web/FuncionarioController.java
+++ src/main/java/com/example/bicicletario/bicicletario/web/FuncionarioController.java
@@ -4,12 +4,9 @@ import com.example.bicicletario.bicicletario.domain.dto.NovoFuncionarioDTO;
 import com.example.bicicletario.bicicletario.application.FuncionarioService;
 import com.example.bicicletario.bicicletario.mapper.FuncionarioMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/funcionario")
